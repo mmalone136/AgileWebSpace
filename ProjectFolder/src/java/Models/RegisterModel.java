c1df4d66d731bbf83ac0356de49a343cc7ecc5af
mmalone136 <mmalone136@gmail.com>
2016-03-16 12:37:05 +0000

    Edit to register nodel to handle trying to sign in a student with an invalid id, will now return false

--- ProjectFolder/src/java/Models/RegisterModel.java
+++ ProjectFolder/src/java/Models/RegisterModel.java
@@ -18,40 +18,41 @@ import java.util.ArrayList;
  * @author Matt
  */
 public class RegisterModel {
+
     public boolean doRegister(int bookingID, String username) {
-		try {
+        try {
 			// TODO: Gain username && password from somewhere
-			//username = "000001";
-			//password = "turkeyBaconSandwich";
-			//ArrayList<String> theStrings = new ArrayList<String>();
-                        //String [] theList = new String[4];
-			
-			
-			String theDriver = "com.mysql.jdbc.Driver";
-			Class driver_class = Class.forName(theDriver);
-			Driver driver = (Driver) driver_class.newInstance();
-			DriverManager.registerDriver(driver);
-			Connection conn = DriverManager.getConnection("jdbc:mysql://silva.computing.dundee.ac.uk:3306/15agileteam2db?" + "user=15agileteam2&password=349.at2.psswd");
-			PreparedStatement ps = conn.prepareStatement("call  sign_in (?,?,?)");
-			ps.setString(1, username);
-                        ps.setInt(2, bookingID);
-			ps.setString(3, "");
-                        
-                        
-                        ResultSet rs = ps.executeQuery();
-                        
-                        
+            //username = "000001";
+            //password = "turkeyBaconSandwich";
+            //ArrayList<String> theStrings = new ArrayList<String>();
+            //String [] theList = new String[4];
+
+            String theDriver = "com.mysql.jdbc.Driver";
+            Class driver_class = Class.forName(theDriver);
+            Driver driver = (Driver) driver_class.newInstance();
+            DriverManager.registerDriver(driver);
+            Connection conn = DriverManager.getConnection("jdbc:mysql://silva.computing.dundee.ac.uk:3306/15agileteam2db?" + "user=15agileteam2&password=349.at2.psswd");
+            PreparedStatement ps = conn.prepareStatement("call  sign_in (?,?,?)");
+            ps.setString(1, username);
+            ps.setInt(2, bookingID);
+            ps.setString(3, "");
+
+            ResultSet rs = ps.executeQuery();
+
+            int i = ps.getUpdateCount();
+
 			//ResultSet rs;
-                        //ps.executeUpdate();
-                        //int i = ps.getUpdateCount();
-                        
-                        return true;
+            //ps.executeUpdate();
+            //int i = ps.getUpdateCount();
+            if (i == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-
-	}
+    }
 }
